eeceda951b7eafa06aa77755fd22327f0e144136
Isabellemew <a.serikbay007@gmail.com>
2025-04-26 23:32:40 +0500

    finally

--- internal/models/products.go
+++ internal/models/products.go
@@ -4,7 +4,7 @@ type Product struct {
     ID         uint   `json:"id" gorm:"primaryKey"`
     Name       string `json:"name"`
     Price      string `json:"price"`
-    ImageURL   string `json:"image_url"`
+    Img   string `json:"img"`
     Brand      string `json:"brand"`
     CategoryID uint   `json:"category_id"`
 }
